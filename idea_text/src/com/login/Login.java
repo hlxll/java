2ddba7f3afbcb661c01307946996305c68773a8b
unknown <huanglin39213@hs.handsome.com.cn>
2021-09-12 21:32:59 +0800

    '更新数组'

--- idea_text/src/com/login/Login.java
+++ idea_text/src/com/login/Login.java
@@ -1,7 +1,42 @@
 package com.login;
+//87
 
+//快捷键
+//psvm生成main方法，sout生产输出语句。Ctrl+Alt+space（内容提示，代码补全等）
+//多行注释Ctrl+Shift+/
 public class Login {
     public static  void  main(String[] args) {
-        System.out.println("helloword");
+        int[] arr = new int[3];
+        int[] arrS = {1,2,3};//静态命名
+
+        System.out.println(arr.length);
+        System.out.println(arrS[0]);
+        for(int x=0; x<3; x++){
+            System.out.println(arrS[x]);
+        }
+        login(2);
+        register(1, 2);
+        register((byte) 10, (byte) 20);
+        int[] arrList = {1,2,3,4,5};
+        arrJoin(arrList);
     }
+    public static void login(int age){
+        System.out.println("方法定义");
+        System.out.println(age);
+    }
+    public static boolean register(int a, int b){
+        System.out.println("int");
+        return a==b;
+    }
+    public static boolean register(byte a, byte b){
+        System.out.println("byte");
+        return a == b;
+    }
+
+    public static void arrJoin(int[] arr) {
+        for (int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    //方法重载，在同一个类中，且方法名相同的，参数不同和返回值不同都是重载
 }
